87ec425c1ac516f7f104524a75969a6f111d4668
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-04-09 12:37:22 -0500

    Add files via upload

---
+++ ReadInput.java
@@ -0,0 +1,46 @@
+import java.io.*;
+
+public class ReadInput {
+    public static void main(String[] args) throws IOException {
+//
+//        byte [] data = new byte[11];
+//        System.out.println("Enter some characters: ");
+//        System.in.read(data);
+//        System.out.println("You entered: ");
+//
+//        for(int i = 0; i < data.length; i++){
+//            System.out.println((char) data[i]);
+//        }
+//
+
+        File file = new File("/Users/elenavoinu/OneDrive - City Colleges of Chicago/udemy/IOReadTextFromFile/src/text"); //("src/test.txt.")
+        try{
+            //FileInputStream is a subclass of input stream so it's safe to
+            // assign an instance of FileInputStream to an InputStream variable
+            InputStream  inputStream = new FileInputStream(file);
+            System.out.println("Total file size in byte is : " + inputStream.available());
+
+            // The read() method of an InputStream returns an int which contains the byte value of the byte read.
+            int content = inputStream.read();
+
+            // while we haven't reached the end of the file (no more bytes to read).
+            //To read all bytes in a Java InputStream you must keep reading until the value -1 is returned.
+            // This value means that there are no more bytes to read from the InputStream.
+            while(content!=-1){
+
+                // read next byte
+                content = inputStream.read();
+
+                //convert to characters and display the content
+                System.out.print((char)content);
+            }
+            inputStream.close();
+        }
+
+        catch(IOException e) {
+
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
